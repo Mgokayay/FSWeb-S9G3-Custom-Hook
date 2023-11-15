2856f4ee2b339432b3e52f4c1ce6e8390b59765b
Mgokayay <aymustafagokay@gmail.com>
2023-11-15 21:53:05 +0300

    gece modu,local storage ve use axios yapıldı

---
+++ frontend/components/hooks/geceModuAc.js
@@ -0,0 +1,10 @@
+import { useLocalStorage } from "./localStorageKullan";
+
+export const useGeceModu = (initialValue) => {
+  const [geceModu, setGeceModu] = useLocalStorage("geceModu", initialValue);
+
+  const changeValue = (value) => {
+    setGeceModu(value);
+  };
+  return [geceModu, changeValue];
+};
